427194eea10a4eac61534ff536f5f5d5dccea6ad
jbirori <jeffbirori@gmail.com>
2017-10-12 17:26:17 -0500

    Basicstructure of the rep cards is set up through state. The next step would be to switch all of the sources from state to props and figure out how to construct the parent component which will pass in the props.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
         quote: "However well-intentioned, Obamacare failed the American people. Illinois families are paying more money for fewer and fewer benefits. Insurance premiums have increased by an average of over 21%. Last year, the Land of Lincoln co-op became the 16th in the country to collapse, leaving 49,000 Illinoisans without any healthcare coverage at all and taxpayers on for hook for over $160 million. We’re committed to an open and transparent process as we debate this legislation.",
         quoteSource: "Press release, March 7, 2017",
         quoteLink: "https://roskam.house.gov/HealthCare",
+        stance: "in-favor",
       }
     }
 
@@ -25,10 +26,10 @@ class App extends Component {
       <div className="App">
         <Grid fluid>
             <Row>
-              <Col xs={3} mdOffset={2} md={2} lgOffset={3} lg={1}>
+              <Col xs={3} smOffset={1} sm={2} mdOffset={2} md={1} lgOffset={3} lg={1}>
                 <img alt="peter" src={this.state.representative.img} className="image"/>
               </Col>
-              <Col xs={9} md={6} lg={5} className="bio">
+              <Col xs={8} sm={8} md={6} lg={5} className="bio">
                 <h4 className="repHeader">
                   {this.state.representative.name},
                   {this.state.representative.party},
