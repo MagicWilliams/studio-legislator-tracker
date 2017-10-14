3ef247ec7a5e1aee47fccbd6428ba72e1d7c8bbe
Sameena Khan <sameenakhan2018@u.northwestern.edu>
2017-10-13 21:04:47 -0500

    Adjusted Card component to use props instead of state

--- src/Stances.js
+++ src/Stances.js
@@ -5,6 +5,7 @@ import Stance from './Stance.js'
 import './Stance.css';
 
 class Stances extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -16,9 +17,18 @@ class Stances extends Component {
     return (
       <div>
       <Stance st="IN FAVOR"/>
-      <Card name="Peter" 
-
+      <Card
+      img="/img/peter.jpg"
+      name="Peter"
+      party=" Republican"
+      state=" IL"
+      district=" 6th District"
+      quote="However well-intentioned, Obamacare failed the American people. Illinois families are paying more money for fewer and fewer benefits. Insurance premiums have increased by an average of over 21%. Last year, the Land of Lincoln co-op became the 16th in the country to collapse, leaving 49,000 Illinoisans without any healthcare coverage at all and taxpayers on for hook for over $160 million. We’re committed to an open and transparent process as we debate this legislation."
+      quoteSource="Press release, March 7, 2017"
+      quoteLink="https://roskam.house.gov/HealthCare"
+      stance="in-favor"
       />
+
       <Stance st="OPPOSED" />
       <Stance st="NO STATEMENT" />
       </div>
