c3d435745df37ba1285e8a548afbe0fcb8810aac
David Latimore <dgl@u.northwestern.edu>
2017-11-07 17:10:57 -0600

    added in doughnut graph using chart js, also need to rename this branch

--- src/components/Graphs/Graphs.jsx
+++ src/components/Graphs/Graphs.jsx
@@ -2,21 +2,30 @@ import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import './Graphs.css';
+import { Doughnut } from 'react-chartjs-2';
 
 class Graphs extends Component {
   render() {
-    return (
-      <div>
-        <Grid className="Banner">
-          <Row>
-            <Col xs={12} className="titleBar">
-              
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    );
+
+    var graphOptions = {
+      animatable: true,
+      legend: {
+        display: false
+      }
+    };
+
+    var data = {
+      datasets: [{
+          data: [251, 276],
+          backgroundColor: ["#5D82DA", "#E47470"],
+          highlight: ["#E47470", "#5D82DA"]
+      }],
+      // LABELS
+      labels: ['Democrat', 'Republican']
+    };
+
+    return <Doughnut className="doughnut" data={data} options={graphOptions}/>
   }
 }
 
-export default Graphs;
+export default Graphs
