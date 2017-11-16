0ff66d2c00c62b923f650c69c3a3354cfe19c773
Sameena Khan <sameenakhan2018@u.northwestern.edu>
2017-11-16 14:48:43 -0600

    Added in feature to filter data by state

--- src/components/SearchByState/SearchByState.jsx
+++ src/components/SearchByState/SearchByState.jsx
@@ -4,44 +4,31 @@ import RepCard from '../RepCard/RepCard';
 import StateReps from '../StateReps/StateReps';
 import './SearchByState.css';
 
+function isIllinois(senator) {
+  return senator.state === ' IL';
+}
+
 class SearchByState extends Component {
   constructor(props) {
     super(props);
     this.state = {
       representatives: props.representatives,
-      currState: "Illinois",
-      senators: [
-        {
-          name: "Durrell Gill",
-          img: "/img/durrell.png",
-          position1: "College Democrats",
-          position2: "college Republicans",
-          email: "durell@u.northwestern.edu",
-        },
-        {
-          name: "Hayeon Kim",
-          img: "/img/durrell2.png",
-          position1: "Asian Pacific",
-          position2: "college Republicans",
-          email: "Hayeon@u.northwestern.edu",
-        },
-        {
-          name: "Karin Zhu",
-          img: "/img/durrell3.png",
-          position1: "College Democrats",
-          position2: "Chinese Students",
-          email: "karin@u.northwestern.edu",
-        },
-        {
-          name: "Dylan Gill",
-          img: "/img/durrell4.png",
-          position1: "College Democrats",
-          position2: "college Republicans",
-          email: "Dylan@u.northwestern.edu",
-        }
-      ]
+      currState: " IL",
     }
+    this.isState = this.isState.bind(this);
   }
+
+  isState(senator) {
+    return senator.state === this.state.currState;
+  }
+
+  // updateStateSelection(state) {
+  //   this.setState({ currState: state });
+    // onClick={this.update};
+    // update() {
+    //    this.props.updateStateSelection(document.getElementyById('state_Selection')); All you are putting as the argument is a string
+    // }
+  // }
 
   render() {
     return (
@@ -57,29 +44,9 @@ class SearchByState extends Component {
 
         </Grid>
 
-        <StateReps representatives={this.props.representatives}/>
+        {/* <SelectState updateStateSelection={this.updateStateSelection} /> */}
 
-        {/*<Grid fluid>
-          <Row>
-            <Col xsOffset={3}></Col>
-            {this.state.senators.map(val =>
-              <Col md={2}>
-                <div className="senateCard">
-                  <img src={val.img} alt="dur" className="senatePic"/>
-                  <div className="senateBio">
-                    <p className="Name"> {val.name} </p>
-                    <img src="/img/Academic.png" alt="committee" className="committeePic"/>
-                    <p className="Position"> {val.position1} </p>
-                    <p className="Position"> {val.position2} </p>
-                    <p className="Position"> {val.email} </p>
-                  </div>
-                </div>
-              </Col>
-            )}
-
-          </Row>
-
-        </Grid>*/}
+        <StateReps representatives={this.state.representatives.filter(this.isState)}/>
 
       </div>
     );
