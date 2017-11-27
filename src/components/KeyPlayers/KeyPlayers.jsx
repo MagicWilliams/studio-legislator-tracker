a5c4d8595d42a6e0d7296ac4736ac046ca62e016
SameenaK <sameenakhan2018@u.northwestern.edu>
2017-11-27 12:20:26 -0600

    Added dummy data to see how things look

--- src/components/KeyPlayers/KeyPlayers.jsx
+++ src/components/KeyPlayers/KeyPlayers.jsx
@@ -35,7 +35,10 @@ class KeyPlayer extends Component {
             state: " IL",
             district: " 1st District",
             quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            stance: "opposed"
+            stance: "opposed",
+            quoteSource: "Press release, March 7, 2017",
+            quoteLink: "http://robinkelly.house.gov/media-center/press-releases/rep-kelly-releases-a-statement-on-republicans-aca-repeal-bill",
+
         },
         {
           name: "Dan Lipinski",
@@ -44,7 +47,10 @@ class KeyPlayer extends Component {
           state: " IL",
           district: " 3rd District",
           quote: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-          stance: "in-favor"
+          stance: "in-favor",
+          quoteSource: "Press release, March 7, 2017",
+          quoteLink: "http://robinkelly.house.gov/media-center/press-releases/rep-kelly-releases-a-statement-on-republicans-aca-repeal-bill",
+
         },
         {
           name: "Danny K. Davis",
@@ -53,7 +59,10 @@ class KeyPlayer extends Component {
           state: " IL",
           district: " 7th District",
           quote: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga",
-          stance: "no-statement"
+          stance: "no-statement",
+          quoteSource: "Press release, March 7, 2017",
+          quoteLink: "http://robinkelly.house.gov/media-center/press-releases/rep-kelly-releases-a-statement-on-republicans-aca-repeal-bill",
+
         },
       ],
       keyPlayer: true
